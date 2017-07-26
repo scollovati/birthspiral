ac34d5abbdbddaadca9c4e8693ce4d3213d8880a
Stefano Collovati <stefano.collovati@gmail.com>
2017-07-26 17:42:12 +0200

    created basic html & js

---
+++ birthspiral.js
@@ -0,0 +1,9 @@
+function setup() {
+  var widthdiv = document.getElementById('p5Canvas').offsetWidth;
+  var myCanvas = createCanvas(widthdiv, 25);
+  myCanvas.parent('p5Canvas');
+}
+
+function draw() {
+  background(255);
+}
