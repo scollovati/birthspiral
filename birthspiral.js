2bc38174b232df2c05216e39b50b97eee72acb06
Stefano Collovati <stefano.collovati@gmail.com>
2017-07-26 17:58:17 +0200

    renamed files

--- birthspiral.js
+++ birthspiral.js
@@ -1,9 +1,10 @@
 function setup() {
-  var widthdiv = document.getElementById("p5Canvas").offsetWidth;
-  var myCanvas = createCanvas(widthdiv, 25);
-  myCanvas.parent("p5Canvas");
+  //var widthdiv = document.getElementById("p5Canvas").offsetWidth;
+  //var myCanvas = createCanvas(widthdiv, 25);
+  //myCanvas.parent("p5Canvas");
 }
 
 function draw() {
   background(123);
+  ellipse(50, 50, 80, 80);
 }
